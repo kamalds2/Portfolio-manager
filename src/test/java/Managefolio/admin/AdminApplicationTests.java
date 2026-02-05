b01624e3bcd36b1415acdfa30f41d426e69ca647
kamalds2 <kamalkumar.doddi@gmail.com>
2026-02-05 20:44:50 +0530

    updated the deploying file

--- src/test/java/Managefolio/admin/AdminApplicationTests.java
+++ src/test/java/Managefolio/admin/AdminApplicationTests.java
@@ -10,8 +10,14 @@ import org.springframework.test.context.TestPropertySource;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 @TestPropertySource(properties = {
-	"spring.jpa.hibernate.ddl-auto=create",
-	"spring.jpa.defer-datasource-initialization=true",
+	"spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+	"spring.datasource.driver-class-name=org.h2.Driver",
+	"spring.datasource.username=sa",
+	"spring.datasource.password=",
+	"spring.jpa.hibernate.ddl-auto=create-drop",
+	"spring.jpa.defer-datasource-initialization=false",
+	"spring.sql.init.mode=never",
+	"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
 	"logging.level.org.hibernate.SQL=ERROR",
 	"logging.level.org.springframework.security=ERROR"
 })
